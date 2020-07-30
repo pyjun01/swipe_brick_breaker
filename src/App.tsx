85878c9756407868f9e26b6e9ad61a4be40b7013
pyjun01 <pyjun02@gmail.com>
2020-07-30 23:00:09 +0900

    React Init

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+let canvas: any;
+let ctx;
+
+const onMouseDown = (event: MouseEvent) => {};
+
+const onMouseMove = (event: MouseEvent) => {};
+
+const onMouseUp = (event: MouseEvent) => {};
+
+function App() {
+  const canvasRef = useRef(null);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [ballCount, setBallCount] = useState(1);
+
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+
+    canvas.addEventListener('mousedown', onMouseDown);
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
+  }, []);
+
+  return (
+    <div className='App'>
+      <div>
+        <div>
+          점수: <span className='value'>{score}</span>
+        </div>
+        <div>
+          최고 점수: <span className='value'>{highScore}</span>
+        </div>
+        <div>
+          Ball: <span className='value'>{ballCount}</span>
+        </div>
+      </div>
+      <canvas ref={canvasRef} id='canvas' width='450' height='450'></canvas>
+    </div>
+  );
+}
+
+export default App;
